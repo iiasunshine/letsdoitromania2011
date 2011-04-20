eab5225111e956727fc5de1fbc54fb0744c766e5
svguna@gmail.com <svguna@gmail.com@61664029-911e-ca19-71c8-b2a769399961>
2011-04-20 19:59:08 +0000

    refactoring of tests

--- Web/Service/LDIRBackendTests/src/ro/ldir/tests/ClosedAreaTest.java
+++ Web/Service/LDIRBackendTests/src/ro/ldir/tests/ClosedAreaTest.java
@@ -1,3 +1,27 @@
+/**
+ *  This file is part of the LDIRBackend - the backend for the Let's Do It
+ *  Romania 2011 Garbage collection campaign.
+ *  Copyright (C) 2011 by the LDIR development team, further referred to 
+ *  as "authors".
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Filename: GarbageManager.java
+ *  Author(s): Stefan Guna, svguna@gmail.com
+ *
+ */
+
 package ro.ldir.tests;
 
 import static org.junit.Assert.assertFalse;
@@ -13,6 +37,9 @@ import org.junit.Test;
 import ro.ldir.dto.ChartedArea;
 import ro.ldir.dto.ClosedArea;
 
+/**
+ * Tester for the {@link ro.ldir.dro.ClosedArea} class.
+ */
 public class ClosedAreaTest {
 	private static ClosedArea arrowHead = new ChartedArea();
 	private static ClosedArea square = new ChartedArea();
@@ -30,16 +57,6 @@ public class ClosedAreaTest {
 	}
 
 	@BeforeClass
-	public static void setupTriangle() throws Exception {
-		List<Point2D.Float> points = new ArrayList<Point2D.Float>();
-		points.add(new Point2D.Float(0, 0));
-		points.add(new Point2D.Float(2, 0));
-		points.add(new Point2D.Float(1, 2));
-		triangle.setPolyline(points);
-		triangle.setBoundingBox();
-	}
-
-	@BeforeClass
 	public static void setupSquare() throws Exception {
 		List<Point2D.Float> points = new ArrayList<Point2D.Float>();
 		points.add(new Point2D.Float(0, 0));
@@ -48,6 +65,16 @@ public class ClosedAreaTest {
 		points.add(new Point2D.Float(0, 2));
 		square.setPolyline(points);
 		square.setBoundingBox();
+	}
+
+	@BeforeClass
+	public static void setupTriangle() throws Exception {
+		List<Point2D.Float> points = new ArrayList<Point2D.Float>();
+		points.add(new Point2D.Float(0, 0));
+		points.add(new Point2D.Float(2, 0));
+		points.add(new Point2D.Float(1, 2));
+		triangle.setPolyline(points);
+		triangle.setBoundingBox();
 	}
 
 	@Test
