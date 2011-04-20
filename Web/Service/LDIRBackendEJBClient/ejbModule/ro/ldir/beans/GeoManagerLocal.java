372cdec4d9a54e376355d0820582eec530a99807
svguna@gmail.com <svguna@gmail.com@61664029-911e-ca19-71c8-b2a769399961>
2011-04-20 19:59:50 +0000

    added administrative areas dto

--- Web/Service/LDIRBackendEJBClient/ejbModule/ro/ldir/beans/GeoManagerLocal.java
+++ Web/Service/LDIRBackendEJBClient/ejbModule/ro/ldir/beans/GeoManagerLocal.java
@@ -1,10 +1,13 @@
 package ro.ldir.beans;
 
+import java.awt.geom.Point2D;
 import java.util.List;
 
 import javax.ejb.Local;
 
 import ro.ldir.dto.ChartedArea;
+import ro.ldir.dto.CountyArea;
+import ro.ldir.dto.TownArea;
 
 @Local
 public interface GeoManagerLocal {
@@ -15,6 +18,40 @@ public interface GeoManagerLocal {
 	 *            The chart area ID to delete.
 	 */
 	public void deleteChartedArea(int chartedAreaId);
+
+	/**
+	 * Deletes a county area.
+	 * 
+	 * @param countyAreaId
+	 *            The county area ID to delete.
+	 */
+	public void deleteCountyArea(int countyAreaId);
+
+	/**
+	 * Deletes a town area.
+	 * 
+	 * @param townAreaId
+	 *            The town area ID to delete.
+	 */
+	public void deleteTownArea(int townAreaId);
+
+	/**
+	 * Finds a chart area by ID.
+	 * 
+	 * @param chartedAreaId
+	 *            The ID of the chart area to search for.
+	 * @return
+	 */
+	public ChartedArea getChartedArea(int chartedAreaId);
+
+	/**
+	 * Returns the chart area containing a given point.
+	 * 
+	 * @param point
+	 *            The point for which the chart area is searched for.
+	 * @return The chart area containing the point.
+	 */
+	public ChartedArea getChartedArea(Point2D.Float point);
 
 	/**
 	 * Gets all the charting areas that intersect a bounding box.
@@ -33,13 +70,40 @@ public interface GeoManagerLocal {
 			float bottomRightX, float bottomRightY);
 
 	/**
-	 * Finds a chart area by ID.
+	 * Finds an county area by ID.
 	 * 
-	 * @param chartedAreaId
-	 *            The ID of the chart area to search for.
+	 * @param countyAreaId
+	 *            The ID of the county area to search for.
 	 * @return
 	 */
-	public ChartedArea getChartedArea(int chartedAreaId);
+	public CountyArea getCountyArea(int countyAreaId);
+
+	/**
+	 * Returns the county area containing a given point.
+	 * 
+	 * @param point
+	 *            The point for which the county area is searched for.
+	 * @return The county area containing the point.
+	 */
+	public CountyArea getCountyArea(Point2D.Float point);
+
+	/**
+	 * Finds an town area by ID.
+	 * 
+	 * @param townAreaId
+	 *            The ID of the town area to search for.
+	 * @return
+	 */
+	public TownArea getTownArea(int townAreaId);
+
+	/**
+	 * Returns the town area containing a given point.
+	 * 
+	 * @param point
+	 *            The point for which the town area is searched for.
+	 * @return The town area containing the point.
+	 */
+	public TownArea getTownArea(Point2D.Float point);
 
 	/**
 	 * Creates a new chart area.
@@ -50,6 +114,22 @@ public interface GeoManagerLocal {
 	public void newChartedArea(ChartedArea chartedArea);
 
 	/**
+	 * Creates a new county area.
+	 * 
+	 * @param countyArea
+	 *            The new county area to create.
+	 */
+	public void newCountyArea(CountyArea countyArea);
+
+	/**
+	 * Creates a new town area.
+	 * 
+	 * @param townArea
+	 *            The new town area to create.
+	 */
+	public void newTownArea(TownArea townArea);
+
+	/**
 	 * Updates a chart area.
 	 * 
 	 * @param chartedAreaId
@@ -58,4 +138,24 @@ public interface GeoManagerLocal {
 	 *            The new settings.
 	 */
 	public void updateChartedArea(int chartedAreaId, ChartedArea chartedArea);
+
+	/**
+	 * Updates an county area.
+	 * 
+	 * @param countyAreaId
+	 *            The county area ID to update.
+	 * @param countyArea
+	 *            The new settings.
+	 */
+	public void updateCountyArea(int countyAreaId, CountyArea countyArea);
+
+	/**
+	 * Updates an town area.
+	 * 
+	 * @param townAreaId
+	 *            The town area ID to update.
+	 * @param townArea
+	 *            The new settings.
+	 */
+	public void updateTownArea(int townAreaId, TownArea townArea);
 }
