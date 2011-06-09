5e3e424176371fea7ef7dc63514260489a0f4ae3
tudor.petru.stefanescu@gmail.com <tudor.petru.stefanescu@gmail.com@61664029-911e-ca19-71c8-b2a769399961>
2011-06-09 06:08:52 +0000

    progress bars

--- Android/src/ro/letsdoitromania/android/main/MormanParams.java
+++ Android/src/ro/letsdoitromania/android/main/MormanParams.java
@@ -11,13 +11,14 @@ import android.widget.ArrayAdapter;
 import android.content.Intent;
 import android.view.View;
 
-public class MormanParams extends Activity {
+import android.widget.SeekBar;
+import android.widget.SeekBar.*;
+
+public class MormanParams extends Activity{
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        chk1 = chk2 = chk3 = chk4 = chk5 = false; 	    
         
         //Restore authenticated session - if any
         setContentView(R.layout.morman);
@@ -36,58 +37,40 @@ public class MormanParams extends Activity {
 			}
 		});
         
-        //checkboxes - chestia asta sigur se poate face cu un ArrayAdapter dar ... pentru moment asta e
-        //TODO - rescrie asta cu arrayadapter or something.
-        
-        ((CheckBox) findViewById(R.id.checkbox_1)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks, depending on whether it's now checked
-                if (((CheckBox) v).isChecked()) {
-                    chk1 = true;//Toast.makeText(HelloFormStuff.this, "Selected", Toast.LENGTH_SHORT).show();
-                } 
-            }
-        });
-        
-        ((CheckBox) findViewById(R.id.checkbox_2)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks, depending on whether it's now checked
-                if (((CheckBox) v).isChecked()) {
-                    chk2 = true;//Toast.makeText(HelloFormStuff.this, "Selected", Toast.LENGTH_SHORT).show();
-                } 
-            }
-        });
-        
-        ((CheckBox) findViewById(R.id.checkbox_3)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks, depending on whether it's now checked
-                if (((CheckBox) v).isChecked()) {
-                    chk3 = true;//Toast.makeText(HelloFormStuff.this, "Selected", Toast.LENGTH_SHORT).show();
-                } 
-            }
-        });
-        
-        ((CheckBox) findViewById(R.id.checkbox_4)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks, depending on whether it's now checked
-                if (((CheckBox) v).isChecked()) {
-                    chk4 = true;//Toast.makeText(HelloFormStuff.this, "Selected", Toast.LENGTH_SHORT).show();
-                } 
-            }
-        });
-        
-        ((CheckBox) findViewById(R.id.checkbox_5)).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on clicks, depending on whether it's now checked
-                if (((CheckBox) v).isChecked()) {
-                    chk5 = true;//Toast.makeText(HelloFormStuff.this, "Selected", Toast.LENGTH_SHORT).show();
-                } 
-            }
-        });
-        
+
+        SeekBar seekBar1 = (SeekBar)findViewById(R.id.seekbar1);
+        setSeekbarListner(seekBar1);
+
+        SeekBar seekBar2 = (SeekBar)findViewById(R.id.seekbar2);
+        setSeekbarListner(seekBar2);
+
+        SeekBar seekBar3 = (SeekBar)findViewById(R.id.seekbar3);
+        setSeekbarListner(seekBar3);
+
+        SeekBar seekBar4 = (SeekBar)findViewById(R.id.seekbar4);
+        setSeekbarListner(seekBar4);
        
     }
     
+    public void setSeekbarListner(SeekBar bar){
+    	bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
 
+    		public void onProgressChanged(SeekBar seekBar, int progress,
+    		  boolean fromUser) {
+    		 // TODO Auto-generated method stub
+    		 //seekBarValue.setText(String.valueOf(progress));
+    		}
+
+    		public void onStartTrackingTouch(SeekBar seekBar) {
+    		 // TODO Auto-generated method stub
+    		}
+
+    		public void onStopTrackingTouch(SeekBar seekBar) {
+    		 // TODO Auto-generated method stub
+    		}
+    	});
+    }
+       
     private void saveAndReturn(){
     	//TODO serializează mormanul - dacă ai semnal, trimite-l, dacă nu salvează-l ca să-l trimiți data viitoare
     	Intent resultIntent = new Intent();
@@ -96,9 +79,4 @@ public class MormanParams extends Activity {
     }
     
     static boolean add;//if the status activity wants to add a new one
-    boolean chk1;
-    boolean chk2;
-    boolean chk3;
-    boolean chk4;
-    boolean chk5;
 }
